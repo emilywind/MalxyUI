5b650293df82efc2ffaa6b453a6c544c3df43d15
Emily Wind <emlywind@gmail.com>
2025-07-27 17:50:26 -0400

    Reformat this

--- src/ObjectiveTracker.lua
+++ src/ObjectiveTracker.lua
@@ -17,20 +17,24 @@ local function skinObjectiveTracker(desaturation)
   end
 end
 
+local function hideObjectiveTracker()
+  local instanceType = select(2, IsInInstance())
+
+  if instanceType == 'pvp' then
+    ObjectiveTrackerFrame:SetAlpha(0)
+    RegisterStateDriver(ObjectiveTrackerFrame, 'visibility', 'hide')
+  else
+    ObjectiveTrackerFrame:SetAlpha(1)
+    RegisterStateDriver(ObjectiveTrackerFrame, 'visibility', 'show')
+  end
+end
+
 local frame = CreateFrame('Frame')
 frame:RegisterEvent('PLAYER_ENTERING_WORLD')
 frame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 frame:SetScript('OnEvent', function()
   if EUIDB.hideObjectiveTracker then
-    local instanceType = select(2, IsInInstance())
-
-    if instanceType == 'pvp' then
-      ObjectiveTrackerFrame:SetAlpha(0)
-      RegisterStateDriver(ObjectiveTrackerFrame, 'visibility', 'hide')
-    else
-      ObjectiveTrackerFrame:SetAlpha(1)
-      RegisterStateDriver(ObjectiveTrackerFrame, 'visibility', 'show')
-    end
+    hideObjectiveTracker()
   end
 
   if EUIDB.darkenUi then
