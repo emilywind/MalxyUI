d9428f697c17b933ef78637cffcac5629323df55
Emily Wind <emlywind@gmail.com>
2025-08-07 23:32:38 -0400

    No longer used

--- src/LossOfControl.lua
+++ src/LossOfControl.lua
@@ -1,17 +1,3 @@
-local cleanIconBackdrop = {
-  bgFile = SQUARE_TEXTURE,
-  edgeFile = SQUARE_TEXTURE,
-  tile = false,
-  tileSize = 0,
-  edgeSize = 3,
-  insets = {
-    left = -2,
-    right = -2,
-    top = -2,
-    bottom = -2
-  }
-}
-
 OnPlayerLogin(function(self, event, ...)
    -- Hide red shadow
   select(2,LossOfControlFrame:GetRegions()):SetAlpha(0)
