9af10ecf7048444f096dadac0c813c58e6f355d1
Emily Wind <emlywind@gmail.com>
2025-08-24 20:24:19 -0400

    Add FasterLoot module to speed up looting

---
+++ src/FasterLoot.lua
@@ -0,0 +1,22 @@
+local tDelay = 0
+
+local function FastLoot()
+  if GetTime() - tDelay >= 0.3 then
+    tDelay = GetTime()
+    if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
+      if TSMDestroyBtn and TSMDestroyBtn:IsShown() and TSMDestroyBtn:GetButtonState() == "DISABLED" then
+        tDelay = GetTime()
+        return
+      end
+      for i = GetNumLootItems(), 1, -1 do
+        LootSlot(i)
+      end
+      tDelay = GetTime()
+    end
+  end
+end
+
+-- Event frame
+local faster = CreateFrame("Frame")
+faster:RegisterEvent("LOOT_READY")
+faster:SetScript("OnEvent", FastLoot)
