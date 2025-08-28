80ff49471a666e6b857bffba5cf06afaa153771d
Emily Wind <emlywind@gmail.com>
2025-08-28 09:40:59 -0400

    Fix indentation

--- src/AutoKeystone.lua
+++ src/AutoKeystone.lua
@@ -1,25 +1,25 @@
 -- Code via LazyKeystones - https://www.curseforge.com/wow/addons/lazy-keystones by thejustinist
 
 local InsertKeystone = function()
-	for bag = 0, NUM_BAG_SLOTS do
-         for bagSlot = 1, GetContainerNumSlots(bag) do
-            local _,_,_,_,_,_,itemName = GetContainerItemInfo(bag, bagSlot)
-            if itemName  ~= nil and string.match(itemName, "Keystone") then
-               if (ChallengesKeystoneFrame:IsShown()) then
-                  PickupContainerItem(bag, bagSlot)
-                  if (CursorHasItem()) then
-                     C_ChallengeMode.SlotKeystone()
-                  end
-               end
-            end
-         end
+  for bag = 0, NUM_BAG_SLOTS do
+    for bagSlot = 1, GetContainerNumSlots(bag) do
+      local _,_,_,_,_,_,itemName = GetContainerItemInfo(bag, bagSlot)
+      if itemName  ~= nil and string.match(itemName, "Keystone") then
+        if (ChallengesKeystoneFrame:IsShown()) then
+          PickupContainerItem(bag, bagSlot)
+          if (CursorHasItem()) then
+            C_ChallengeMode.SlotKeystone()
+          end
+        end
       end
+    end
+  end
 end
 
 OnEvent("ADDON_LOADED", function(self, event, addonName)
-    if event == "ADDON_LOADED" and addonName == "Blizzard_ChallengesUI" then
-		ChallengesKeystoneFrame:HookScript("OnShow", InsertKeystone)
-        self:UnregisterEvent("ADDON_LOADED")
-        return
-	end
+  if event == "ADDON_LOADED" and addonName == "Blizzard_ChallengesUI" then
+    ChallengesKeystoneFrame:HookScript("OnShow", InsertKeystone)
+    self:UnregisterEvent("ADDON_LOADED")
+    return
+  end
 end)
