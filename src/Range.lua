357685f494681d647a7f78db15c290173842b6eb
Emily Wind <emlywind@gmail.com>
2025-08-30 12:49:35 -0400

    Update Range.lua to be functional instead of object-oriented for better readability and flow with the rest of the project

--- src/Range.lua
+++ src/Range.lua
@@ -1,9 +1,8 @@
 OnPlayerLogin(function()
-  local Module = CreateFrame('Frame')
-
   local UPDATE_DELAY = .2
   local buttonColors, buttonsToUpdate = {}, {}
   local updater = CreateFrame("Frame")
+  local timeElapsed = 0
 
   local colors = {
     ["normal"] = {1, 1, 1},
@@ -12,22 +11,22 @@ OnPlayerLogin(function()
     ["unusable"] = {.3, .3, .3}
   }
 
-  function Module:OnUpdateRange(elapsed)
-    self.elapsed = (self.elapsed or UPDATE_DELAY) - elapsed
-    if self.elapsed <= 0 then
-      self.elapsed = UPDATE_DELAY
+  function OnUpdateRange(self, elapsed)
+    timeElapsed = (timeElapsed or UPDATE_DELAY) - elapsed
+    if timeElapsed <= 0 then
+      timeElapsed = UPDATE_DELAY
 
-      if not Module:UpdateButtons() then
+      if not UpdateButtons() then
         self:Hide()
       end
     end
   end
-  updater:SetScript("OnUpdate", Module.OnUpdateRange)
+  updater:SetScript("OnUpdate", OnUpdateRange)
 
-  function Module:UpdateButtons()
+  function UpdateButtons()
     if next(buttonsToUpdate) then
       for button in pairs(buttonsToUpdate) do
-        self.UpdateButtonUsable(button)
+        UpdateButtonUsable(button)
       end
       return true
     end
@@ -35,59 +34,59 @@ OnPlayerLogin(function()
     return false
   end
 
-  function Module:UpdateButtonStatus()
-    local action = self.action
+  function UpdateButtonStatus(button)
+    local action = button.action
 
-    buttonsToUpdate[self] = (action and self:IsVisible() and HasAction(action)) and true or nil
+    buttonsToUpdate[button] = (action and button:IsVisible() and HasAction(action)) and true or nil
 
     if next(buttonsToUpdate) then
       updater:Show()
     end
   end
 
-  function Module:UpdateButtonUsable(force)
+  function UpdateButtonUsable(button, force)
     if force then
-      buttonColors[self] = nil
+      buttonColors[button] = nil
     end
 
-    local action = self.action
+    local action = button.action
     local isUsable, notEnoughMana = IsUsableAction(action)
 
     if isUsable then
       local inRange = IsActionInRange(action)
-      Module.SetButtonColor(self, inRange == false and "oor" or "normal")
+      SetButtonColor(button, inRange == false and "oor" or "normal")
     elseif notEnoughMana then
-      Module.SetButtonColor(self, "oom")
+      SetButtonColor(button, "oom")
     else
-      Module.SetButtonColor(self, "unusable")
+      SetButtonColor(button, "unusable")
     end
   end
 
-  function Module:SetButtonColor(colorIndex)
-    if buttonColors[self] == colorIndex then
+  function SetButtonColor(button, colorIndex)
+    if buttonColors[button] == colorIndex then
       return
     end
-    buttonColors[self] = colorIndex
+    buttonColors[button] = colorIndex
 
     local r, g, b = unpack(colors[colorIndex])
-    self.icon:SetVertexColor(r, g, b)
+    button.icon:SetVertexColor(r, g, b)
   end
 
-  function Module:Register()
-    self:HookScript("OnShow", Module.UpdateButtonStatus)
-    self:HookScript("OnHide", Module.UpdateButtonStatus)
-    self:SetScript("OnUpdate", nil)
-    Module.UpdateButtonStatus(self)
+  function Register(button)
+    button:HookScript("OnShow", UpdateButtonStatus)
+    button:HookScript("OnHide", UpdateButtonStatus)
+    button:SetScript("OnUpdate", nil)
+    UpdateButtonStatus(button)
   end
 
   local function button_UpdateUsable(button)
-    Module.UpdateButtonUsable(button, true)
+    UpdateButtonUsable(button, true)
   end
 
   local function registerButtonRange(button)
     if button.Update then
-      Module.Register(button)
-      hooksecurefunc(button, "Update", Module.UpdateButtonStatus)
+      Register(button)
+      hooksecurefunc(button, "Update", UpdateButtonStatus)
       hooksecurefunc(button, "UpdateUsable", button_UpdateUsable)
     end
   end
