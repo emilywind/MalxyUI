be79fbfed9efe5decc8c028981c8829fe2823a22
Emily Wind <emlywind@gmail.com>
2025-09-14 04:20:06 -0400

    Just use nameplate variable name, and don't set a variable

--- src/BigDebuffsSkin.lua
+++ src/BigDebuffsSkin.lua
@@ -1,15 +1,12 @@
 OnPlayerLogin(function()
   if not C_AddOns.IsAddOnLoaded('BigDebuffs') then return end
 
-  -- Nameplates
-  hooksecurefunc(BigDebuffs, 'NAME_PLATE_UNIT_ADDED', function(self, _, unit)
-    local namePlate = GetSafeNameplate(unit)
-    if not namePlate or namePlate:IsForbidden() then return end
+  hooksecurefunc(BigDebuffs, 'NAME_PLATE_UNIT_ADDED', function(_, _, unit)
+    local nameplate = GetSafeNameplate(unit)
+    if not nameplate or nameplate:IsForbidden() then return end
 
-    local bdbNameplate = namePlate.BigDebuffs
-
-    if bdbNameplate then
-      ApplyEuiBackdrop(bdbNameplate)
+    if nameplate.BigDebuffs then
+      ApplyEuiBackdrop(nameplate.BigDebuffs)
     end
   end)
 end)
