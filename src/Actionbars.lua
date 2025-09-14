e82069386bc98fbe4b7302af0ee5f843e835bc03
Emily Wind <emlywind@gmail.com>
2025-09-14 15:07:22 -0400

    Ensure that action bar skinning hooks are only applied once, and allow re-skinning of action bars

--- src/Actionbars.lua
+++ src/Actionbars.lua
@@ -1,6 +1,5 @@
 local function applyEuiButtonSkin(bu)
   if not bu then return end
-  if bu.euiClean then return bu.border end
 
   local nt = bu:GetNormalTexture()
 
@@ -8,91 +7,106 @@ local function applyEuiButtonSkin(bu)
 
   ApplyUIMode(nt)
 
-  bu.euiClean = true
+  return bu.border
 end
 
-local function init()
-  --style extraactionbutton
-  local function styleExtraActionButton(bu)
-    if not bu or (bu and bu.euiClean) then return end
+local function styleExtraActionButton(bu)
+  if not bu then return end
 
-    local icon = bu.icon or bu.Icon
+  local icon = bu.icon or bu.Icon
 
-    --icon
-    StyleIcon(icon)
+  StyleIcon(icon)
 
-    --apply background
-    applyEuiButtonSkin(bu)
+  applyEuiButtonSkin(bu)
+end
+
+local function styleActionButton(bu)
+  if not bu then return end
+
+  local name = bu:GetName()
+  local na = _G[name .. "Name"]
+  local nt = _G[name .. "NormalTexture"]
+
+  if EUIDB.hideMacroText then
+    na:Hide()
   end
 
-  --initial style func
-  local function styleActionButton(bu)
-    if not bu or (bu and bu.euiClean) then
-      return
-    end
-    local name = bu:GetName()
-    local na = _G[name .. "Name"]
-    local nt = _G[name .. "NormalTexture"]
-
-    -- Macro name
-    if EUIDB.hideMacroText then
-      na:Hide()
-    end
-
-    if not nt then
-      --fix the non existent texture problem (no clue what is causing this)
-      nt = bu:GetNormalTexture()
-    end
-
-    applyEuiButtonSkin(bu)
+  if not nt then
+    nt = bu:GetNormalTexture()
   end
 
-  -- Style stance buttons
-  for i = 1, StanceBar.numButtons do
-    styleActionButton(_G["StanceButton" .. i])
+  applyEuiButtonSkin(bu)
+end
+
+local function stylePossessButton(bu)
+  if not bu then return end
+
+  local name = bu:GetName()
+  local nt = _G[name .. "NormalTexture"]
+  nt:SetAllPoints(bu)
+
+  applyEuiButtonSkin(bu)
+end
+
+local function updateHotkey(self)
+  local ho = _G[self:GetName() .. "HotKey"]
+  if ho and ho:IsShown() then
+    ho:Hide()
+  end
+end
+
+local function skinSpellFlyout()
+  for _, texture in pairs({
+    SpellFlyout.Background.Start,
+    SpellFlyout.Background.VerticalMiddle,
+    SpellFlyout.Background.HorizontalMiddle,
+    SpellFlyout.Background.End,
+  }) do
+    ApplyUIMode(texture)
   end
 
-  -- Style possess buttons
-  local function stylePossessButton(bu)
-    if not bu or (bu and bu.euiClean) then
-      return
+  local i = 1
+  while (true) do
+    local btnTexture = _G["SpellFlyoutPopupButton" .. i .. "NormalTexture"]
+
+    if not btnTexture then
+      break
     end
-    local name = bu:GetName()
-    local nt = _G[name .. "NormalTexture"]
-    nt:SetAllPoints(bu)
 
-    applyEuiButtonSkin(bu)
+    ApplyUIMode(btnTexture)
+
+    i = i + 1
   end
+end
 
-  --update hotkey func
-  local function updateHotkey(self, actionButtonType)
-    local ho = _G[self:GetName() .. "HotKey"]
-    if ho and ho:IsShown() then
-      ho:Hide()
-    end
-  end
-
+local function applyHooks()
   if EUIDB.hideHotkeys then
-    OnEvents({
-      "UPDATE_BINDINGS",
-      "PLAYER_LOGIN"
-    }, function()
+    local function hideHotKeys()
       for i = 1, 12 do
-        updateHotkey(_G["ActionButton"..i])
-        updateHotkey(_G["MultiBarBottomLeftButton"..i])
-        updateHotkey(_G["MultiBarBottomRightButton"..i])
-        updateHotkey(_G["MultiBarLeftButton"..i])
-        updateHotkey(_G["MultiBarRightButton"..i])
+        updateHotkey(_G["ActionButton" .. i])
+        updateHotkey(_G["MultiBarBottomLeftButton" .. i])
+        updateHotkey(_G["MultiBarBottomRightButton" .. i])
+        updateHotkey(_G["MultiBarLeftButton" .. i])
+        updateHotkey(_G["MultiBarRightButton" .. i])
       end
       for i = 1, 10 do
-        updateHotkey(_G["StanceButton"..i])
-        updateHotkey(_G["PetActionButton"..i])
+        updateHotkey(_G["StanceButton" .. i])
+        updateHotkey(_G["PetActionButton" .. i])
       end
       updateHotkey(ExtraActionButton1)
-    end)
+    end
+
+    OnEvent("UPDATE_BINDINGS", hideHotKeys)
+
+    hideHotKeys()
   end
 
-  --style the actionbar buttons
+  SpellFlyout:HookScript("OnSizeChanged", function()
+    skinSpellFlyout()
+  end)
+end
+
+function StyleActionBars()
   for i = 1, NUM_ACTIONBAR_BUTTONS do
     styleActionButton(_G["ActionButton" .. i])
     styleActionButton(_G["MultiBarBottomLeftButton" .. i])
@@ -107,51 +121,28 @@ local function init()
   for i = 1, 6 do
     styleActionButton(_G["OverrideActionBarButton" .. i])
   end
-  --petbar buttons
   for i = 1, NUM_PET_ACTION_SLOTS do
     styleActionButton(_G["PetActionButton" .. i])
   end
-  --possess buttons
   for i = 1, NUM_POSSESS_SLOTS do
     stylePossessButton(_G["PossessButton" .. i])
   end
 
-  --extraactionbutton1
   styleExtraActionButton(ExtraActionButton1)
 
-  local function skinSpellFlyout()
-    -- Main frame.
-    for _, texture in pairs({
-      SpellFlyout.Background.Start,
-      SpellFlyout.Background.VerticalMiddle,
-      SpellFlyout.Background.HorizontalMiddle,
-      SpellFlyout.Background.End,
-    }) do
-      ApplyUIMode(texture)
-    end
-
-    -- Button borders.
-    local i = 1
-    while (true) do
-      local btnTexture = _G["SpellFlyoutPopupButton" .. i .. "NormalTexture"]
-
-      if not btnTexture then
-        break
-      end
-
-      ApplyUIMode(btnTexture)
-
-      i = i + 1
-    end
+  for i = 1, StanceBar.numButtons do
+    styleActionButton(_G["StanceButton" .. i])
   end
-
-  SpellFlyout:HookScript("OnSizeChanged", function()
-    skinSpellFlyout()
-  end)
 end
 
 OnEvents({
   "PLAYER_LOGIN",
   "PLAYER_SPECIALIZATION_CHANGED",
   "EDIT_MODE_LAYOUTS_UPDATED"
-}, init)
+}, function(_, event)
+  if event == "PLAYER_LOGIN" then
+    applyHooks()
+  end
+
+  StyleActionBars()
+end)
