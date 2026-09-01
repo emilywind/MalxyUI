51e5f7055940e29994fcd03e9254713f1405d701
Emily Wind <emlywind@gmail.com>
2026-08-31 20:38:50 -0400

    Cleaner code for the fix

--- src/Tooltips.lua
+++ src/Tooltips.lua
@@ -116,15 +116,13 @@ function InitTooltips()
 
 		local specNames = LibFroznFunctions:CreatePushArray()
 
-		if (hideSpecializationAndClassText) then
-			if not issecretvalue(unitInfo.classID) then
-				local specCount = C_SpecializationInfo.GetNumSpecializationsForClassID(unitInfo.classID)
+		if (hideSpecializationAndClassText and not issecretvalue(unitInfo.classID)) then
+			local specCount = C_SpecializationInfo.GetNumSpecializationsForClassID(unitInfo.classID)
 
-				for i = 1, specCount do
-					local _, specName = GetSpecializationInfoForClassID(unitInfo.classID, i, unitInfo.sex)
+			for i = 1, specCount do
+				local _, specName = GetSpecializationInfoForClassID(unitInfo.classID, i, unitInfo.sex)
 
-					specNames:Push(specName)
-				end
+				specNames:Push(specName)
 			end
 		end
 
